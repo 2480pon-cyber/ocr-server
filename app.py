646e021ad3511d24a53b8c4582b928b70d3af50b
2480pon-cyber <2480pon@gmail.com>
2026-04-05 00:38:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "OK"
+
+@app.route("/ocr", methods=["POST"])
+def ocr():
+    if "file" not in request.files:
+        return "No file", 400
+
+    file = request.files["file"]
+    
+    # とりあえず確認用
+    return jsonify({
+        "filename": file.filename
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
